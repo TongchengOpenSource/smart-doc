5bb5f11fec4b249ca11dc4bc20b85894dafea448
oppofind <836575280@qq.com>
2019-10-18 00:13:55 +0800

    update DocUtilTest

--- src/test/java/com/power/doc/util/DocUtilTest.java
+++ src/test/java/com/power/doc/util/DocUtilTest.java
@@ -1,5 +1,7 @@
 package com.power.doc.util;
 
+import com.power.doc.constants.DocGlobalConstants;
+import com.power.doc.constants.DocLanguage;
 import com.power.doc.utils.DocUtil;
 import org.junit.Test;
 
@@ -10,7 +12,8 @@ public class DocUtilTest {
 
     @Test
     public void test() {
-        String str = DocUtil.getValByTypeAndFieldName("LocalDateTime", "createTime");
+        System.setProperty(DocGlobalConstants.DOC_LANGUAGE, DocLanguage.CHINESE.getCode());
+        String str = DocUtil.getValByTypeAndFieldName("string", "hostName");
         System.out.println(str);
     }
 }
