7698cde1f9c6e499605fda5c74b0fdf8dd86ebd8
duanbo <boazy@qq.com>
2020-04-12 11:08:02 +0800

    Fix JavaDocBuildTemplate request parameter is a generic type resolution problem

--- src/main/java/com/power/doc/template/JavaDocBuildTemplate.java
+++ src/main/java/com/power/doc/template/JavaDocBuildTemplate.java
@@ -170,7 +170,7 @@ public class JavaDocBuildTemplate {
                         .setType("string").setDesc(comment).setRequired(required).setVersion(DocGlobalConstants.DEFAULT_VERSION);
                 paramList.add(param);
             } else {
-                paramList.addAll(ParamsBuildHelper.buildParams(fullTypeName, DocGlobalConstants.ENMPTY, 0, "true", responseFieldMap, Boolean.FALSE, new HashMap<>(), builder, groupClasses));
+                paramList.addAll(ParamsBuildHelper.buildParams(typeName, DocGlobalConstants.ENMPTY, 0, "true", responseFieldMap, Boolean.FALSE, new HashMap<>(), builder, groupClasses));
             }
         }
         return paramList;
