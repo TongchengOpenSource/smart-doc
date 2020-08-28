a01b868087d43db733074b939346cbdd7dd8f958
xingzi <1771761586@qq.com>
2020-08-28 20:44:04 +0800

    修改isclassname方法

--- src/main/java/com/power/doc/utils/DocClassUtil.java
+++ src/main/java/com/power/doc/utils/DocClassUtil.java
@@ -28,6 +28,7 @@ import com.power.doc.model.ApiReturn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Description:
@@ -237,17 +238,17 @@ public class DocClassUtil {
                 return typeName;
         }
     }
-
+    
     private static boolean isClassName(String className) {
-        if (StringUtil.isEmpty(className)) {
-            return false;
+        className = className.replaceAll("[^<>]", "");
+        Stack<Character> stack = new Stack<>();
+        for (char c : className.toCharArray()) {
+            if (c == '<') {
+                stack.push('>');
+            } else if (stack.isEmpty() || c != stack.pop()) {
+                return false;
+            }
         }
-        if (className.contains("<") && !className.contains(">")) {
-            return false;
-        } else if (className.contains(">") && !className.contains("<")) {
-            return false;
-        } else {
-            return true;
-        }
+        return stack.isEmpty();
     }
 }
