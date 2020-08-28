624d2d974bb7bc98d6bb583fa3dfe6563d90b99f
oppofind <836575280@qq.com>
2020-08-28 22:31:31 +0800

    fix isClassName method error

--- src/main/java/com/power/doc/utils/DocClassUtil.java
+++ src/main/java/com/power/doc/utils/DocClassUtil.java
@@ -28,6 +28,7 @@ import com.power.doc.model.ApiReturn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Description:
@@ -239,16 +240,15 @@ public class DocClassUtil {
     }
 
     private static boolean isClassName(String className) {
-        if (StringUtil.isEmpty(className)) {
-            return false;
+        className = className.replaceAll("[^<>]", "");
+        Stack<Character> stack = new Stack<>();
+        for (char c : className.toCharArray()) {
+            if (c == '<') {
+                stack.push('>');
+            } else if (stack.isEmpty() || c != stack.pop()) {
+                return false;
+            }
         }
-        if (className.contains("<") && !className.contains(">")) {
-            return false;
-        } else if (className.contains(">") && !className.contains("<")) {
-            return false;
-        } else {
-            return true;
-        }
+        return stack.isEmpty();
     }
 }
-
