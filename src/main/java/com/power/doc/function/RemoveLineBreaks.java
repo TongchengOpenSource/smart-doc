c563680c8afb77388eb44c0aa27f81694fdc6f8e
shalousun <smartdoc647@gmail.com>
2021-07-24 01:04:29 +0800

    Add RemoveLineBreaks function of beetl

---
+++ src/main/java/com/power/doc/function/RemoveLineBreaks.java
@@ -0,0 +1,17 @@
+package com.power.doc.function;
+
+import org.beetl.core.Context;
+import org.beetl.core.Function;
+
+/**
+ * @author yu 2021/7/24.
+ */
+public class RemoveLineBreaks implements Function {
+
+  @Override
+  public String call(Object[] paras, Context ctx) {
+    String str = String.valueOf(paras[0]).replaceAll("\n", " ")
+        .replaceAll("\r"," ");
+    return str;
+  }
+}
