3021fe111eeac7496b2e028be45204d31314c955
shalousun <836575280@qq.com>
2024-06-19 23:32:03 +0800

    fix: Fix the issue where the generated OpenAPI is not sorted according to the specified order.

--- src/main/java/com/ly/doc/model/DocMapping.java
+++ src/main/java/com/ly/doc/model/DocMapping.java
@@ -33,32 +33,23 @@ public class DocMapping {
 
     public static  Map<String, TagDoc> TAG_DOC = new ConcurrentHashMap<>(64);
 
-    public static Set<ApiDoc> CLAZZ_DOCS = Collections.synchronizedSet(new LinkedHashSet<>(64));
-
-    public static  Set<ApiMethodDoc> METHOD_DOCS = Collections.synchronizedSet(new LinkedHashSet<>(1024));
-
     public static void tagDocPut(String tag, ApiDoc apiDoc, ApiMethodDoc methodDoc) {
         if (StringUtils.isBlank(tag)) {
             return;
         }
-//        tag = StringUtils.trim(tag);
         TagDoc tagDoc = TAG_DOC.computeIfAbsent(tag, TagDoc::new);
         if (Objects.nonNull(apiDoc)) {
             apiDoc.getTagRefs().add(tagDoc);
             tagDoc.getClazzDocs().add(apiDoc);
-            CLAZZ_DOCS.add(apiDoc);
         }
         if (Objects.nonNull(methodDoc)) {
             methodDoc.getTagRefs().add(tagDoc);
             tagDoc.getMethodDocs().add(methodDoc);
-            METHOD_DOCS.add(methodDoc);
         }
     }
 
     public static void init() {
         TAG_DOC = new ConcurrentHashMap<>(64);
-        CLAZZ_DOCS = Collections.synchronizedSet(new LinkedHashSet<>(64));
-        METHOD_DOCS = Collections.synchronizedSet(new LinkedHashSet<>(1024));
     }
 
 }
