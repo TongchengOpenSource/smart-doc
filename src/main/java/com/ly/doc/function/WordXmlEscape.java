5c36b5d70067a50bbd445a594648953c2212e957
Kien Dinh <kienmatu1@gmail.com>
2025-04-12 20:55:53 +0700

    feat: refactor function package: HtmlEscape and WordXmlEscape

--- src/main/java/com/ly/doc/function/WordXmlEscape.java
+++ src/main/java/com/ly/doc/function/WordXmlEscape.java
@@ -20,27 +20,50 @@
  */
 package com.ly.doc.function;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import com.ly.doc.utils.DocUtil;
 import org.beetl.core.Context;
 import org.beetl.core.Function;
 
 /**
- * beetl template function
+ * Function to escape XML content in Beetl templates.
+ * <p>
+ * For example, it replaces: - <code>" "</code> with <code> </code>
  *
  * @author yu 2021/6/26.
  */
 public class WordXmlEscape implements Function {
 
+	/*
+	 * Represents a non-breaking space character (U+00A0) commonly used in Word documents.
+	 * In HTML, this character is typically represented as "&nbsp;".
+	 */
+	private static final String UNICODE_NON_BREAK_SPACE = "\u00A0";
+
+	private static final Map<String, String> XML_ESCAPE_MAP;
+
+	static {
+		XML_ESCAPE_MAP = new LinkedHashMap<>();
+		XML_ESCAPE_MAP.put(" ", UNICODE_NON_BREAK_SPACE);
+		XML_ESCAPE_MAP.put("&nbsp;&nbsp;", UNICODE_NON_BREAK_SPACE);
+		XML_ESCAPE_MAP.put("&nbsp;", "");
+		XML_ESCAPE_MAP.put("<br/>", "");
+	}
+
 	@Override
-	public String call(Object[] paras, Context ctx) {
-		String str = String.valueOf(paras[0])
-			.replaceAll(" ", " ")
-			.replaceAll("&nbsp;&nbsp;", " ")
-			.replaceAll("&nbsp;", "")
-			.replaceAll("<br/>", "")
-			.replaceAll("&", "&amp;")
-			.replaceAll("<", "&lt;");
-		return DocUtil.getEscapeAndCleanComment(str);
+	public String call(Object[] params, Context ctx) {
+		if (params == null || params.length == 0 || params[0] == null) {
+			return "";
+		}
+
+		String xml = String.valueOf(params[0]);
+		for (Map.Entry<String, String> entry : XML_ESCAPE_MAP.entrySet()) {
+			xml = xml.replaceAll(entry.getKey(), entry.getValue());
+		}
+
+		return DocUtil.getEscapeAndCleanComment(xml);
 	}
 
 }
